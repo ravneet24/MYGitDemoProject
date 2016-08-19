80eab506fa120e56d58f6777fe7b8a2feb5c53a7
ravneet arora <ravneet.arora@a1-q-d02.local>
2016-08-20 01:28:38 +0530

    Mitra da pehla Change

--- MyGitDemo/ViewController.swift
+++ MyGitDemo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Mitra da Pehla Change
     }
 
     override func didReceiveMemoryWarning() {
